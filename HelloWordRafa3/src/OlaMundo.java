2d30215be8a4afe372dea24e0415d8cffbc6ebea
Aluno <Aluno@10.85.198.36>
2024-03-23 13:22:08 -0300

    34 dentro

--- HelloWordRafa3/src/OlaMundo.java
+++ HelloWordRafa3/src/OlaMundo.java
@@ -2,7 +2,7 @@
 public class OlaMundo {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Word");
+		System.out.println("Hello Word 34");
 		// TODO Auto-generated method stub
 
 	}
